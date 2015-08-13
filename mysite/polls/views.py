323fcab3d0d5121d14ca9303cd6a8a5489934d25
David Gibbons <david.gibbons@ammeon.com>
2015-08-13 10:45:03 +0100

    Change a comment

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect  # , HttpResponse
+from django.http import HttpResponseRedirect  # ,HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone
